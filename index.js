9409ead94ce206e2cc0ecfdb8d8b03f03c4b19ab
Superlilosint <maheshmok123@gmail.com>
2026-03-08 21:09:39 +0545

    "Added delete, mark-in-progress, mark-done and list all functionality"

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ function compareNumber(a, b) {
 }
 
 function getNextId(tasks) {
-
     const ids = tasks.map((task) => task.id); // returns the array of id 
     ids.sort(compareNumber); // sort the array of id in ascending order
     let nextId = 1; 
@@ -53,7 +52,7 @@ function addTask(description) {
   );
 }
 
-//Function to update a new task 
+//Function to update a task 
 function updateTask(ids, description) {
     const tasks = readTasks();
     const task = tasks.find(t => t.id === parseInt(ids)); // we shouldn't use === here otherwise undefined answer or use parseInt
@@ -61,12 +60,82 @@ function updateTask(ids, description) {
     if(task) {
         task.description = description;
         task.updatedAt = Date.now();
+        writeTasks(task);
         console.log(`Task added successfully! (ID: ${task.id} and UpdateAt: ${task.updatedAt})`);
     } else {
         console.log(`This ${ids} doesn't exist at all!!`)
     }
 }
 
+//Function to delete a task
+function deleteTask(ids) {
+    const tasks = readTasks();
+
+    const remainTask = tasks.filter(obj => obj.id !== parseInt(ids));
+
+    if(remainTask.length < tasks.length) {
+        writeTasks(remainTask);
+        console.log(`Successfully deleted ${ids}`);
+    } else {
+        console.log(`This id ${ids} doesn't exists!!`);
+    }
+}
+
+//Function mark-in-progress
+function MarkInProgress(ids) {
+    const tasks = readTasks();
+    const markedTask = tasks.find(t => t.id === parseInt(ids));
+    
+    if(markedTask) {
+        markedTask.inProgress = true;
+        markedTask.completed = false;
+        writeTasks(markedTask);
+        console.log(`Successfully marked the progress for this ${markedTask.id} id.`);
+    } else {
+        console.log(`${ids} doesn't exists at all.`)
+    }
+}
+
+//Function mark-done
+function MarkDone(ids) {
+    const tasks = readTasks();
+    const doneTask = tasks.find(t => t.id === parseInt(ids));
+
+    if(doneTask) {
+        doneTask.completed = true;
+        doneTask.inProgress = false;
+        writeTasks(doneTask);
+        console.log(`Successfully marked the task to done for this ${doneTask.id} id.`);
+    } else {
+        console.log(`${ids} doesn't exists at all.`)
+    }
+}
+
+//Function list all
+function listall() {
+    const tasks = readTasks();
+    console.log(tasks);
+}
+
+
+//Function list by status
+function listStatus(status) {
+    const tasks = readTasks();
+
+    let filteredTasks = tasks;
+
+    if(status) {
+        if(status.toLowerCase() === "done") {
+            filteredTasks = tasks.filter((t) => t.completed);
+        }
+    }
+
+    if(filteredTasks.length === 0 ) {
+        console.log("No tasks found.");
+    } else {
+        console.log(filteredTasks);
+    }
+}
 const arg = process.argv.slice(2);
 
 switch (arg[0]) {
@@ -80,6 +149,40 @@ switch (arg[0]) {
         const [command, id, description] = arg;
         console.log("Update command is activated!!");
         updateTask(id, description);
+        break;
+
+    case "delete":
+        const ids = arg[1];
+        console.log("Delete command is active!!")
+        deleteTask(ids);
+        break;
+
+    case "mark-in-progress": 
+        const markId = arg[1];
+        if(!markId) {
+            console.log("Please provide a task ID. ");
+            console.log("Sample: hello-nuru mark-in-progress 1");
+        }
+        MarkInProgress(markId);
+        break;
+
+    case "mark-done": 
+        const doneId = arg[1];
+            if(!doneId) {
+            console.log("Please provide a task ID. ");
+            console.log("Sample: hello-nuru mark-done 1");
+        }
+        MarkDone(doneId);
+        break;
+
+    case "list": 
+        listall();
+        break;
+
+    case "list-status":
+        const status = arg[1];
+        listStatus(status); 
+        //Note: Append is needed because writeTask() is rewritting the answer
         break;
     default:
         console.log(`Sorry, we don't have that command !!`);
